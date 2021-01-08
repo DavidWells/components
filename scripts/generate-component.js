9f14a777be3196f53a9575c93eb94aa67d93fa22
DavidWells <>
2021-01-08 12:05:13 -0800

    chore: clean up templates
    Signed-off-by: DavidWells <>

--- scripts/generate-component.js
+++ scripts/generate-component.js
@@ -2,40 +2,41 @@ const path = require('path')
 const fs = require('fs')
 const copyTemplate = require('copy-template-dir')
 const argv = require('minimist')(process.argv.slice(2));
-const componentName = argv.name
-const componentType = argv.type
+const packageName = argv.name
+const templateType = argv.type
 var templateDir
 
-if (!componentName) {
-  console.log('Error! Must pass in component name. --name=ComponentName')
+if (!packageName) {
+  console.log('Error! Must pass in component name. --name=packageName')
   return false
 }
 
-const outputDir = path.join(__dirname, `../packages/${componentName}`)
-
-if (componentType) {
-  templateDir = path.join(__dirname, `../templates/${componentType}`)
+const outputDir = path.join(__dirname, `../packages/${packageName}`)
+console.log('templateType', templateType)
+if (templateType) {
+  templateDir = path.join(__dirname, `../templates/${templateType}`)
 } else {
   templateDir = path.join(__dirname, `../templates/component`)
 }
 
 /* if directory exists delete it first */
 if (fs.existsSync(outputDir)) {
-  console.log(`${componentName} directory already exists.`)
-  console.log(`Delete ${componentName} directory manually to generate this component`)
+  console.log(`${packageName} directory already exists.`)
+  console.log(`Delete ${packageName} directory manually to generate this component`)
   return false
 }
 
 const templateVariables = {
-  ComponentName: componentName,
-  ComponentNameLowerCase: componentName.toLowerCase()
+  packageName: packageName,
+  ComponentName: packageName,
+  ComponentNameLowerCase: packageName.toLowerCase()
 }
 
 copyTemplate(templateDir, outputDir, templateVariables, (err, createdFiles) => {
   if (err) throw err
 
   createdFiles.forEach((filePath) => {
-    var newName = filePath.replace('ComponentName', componentName)
+    var newName = filePath.replace('ComponentName', packageName)
     fs.rename(`${filePath}`, newName, function(err) {
       if (err) {
         console.log('ERROR: ' + err)
