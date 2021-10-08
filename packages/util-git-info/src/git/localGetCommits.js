6ddcd4f340622245e05c1920b921da7f06593eae
David Wells <>
2021-10-08 01:06:21 -0700

    fix: add failsafe for commits/bodys with wierd characters
    Signed-off-by: David Wells <>

--- packages/util-git-info/src/git/localGetCommits.js
+++ packages/util-git-info/src/git/localGetCommits.js
@@ -18,7 +18,7 @@ const body = '%b'
 const author = `"author": {"name": "${authorName}", "email": "${authorEmail}" }`
 const committer = `"committer": {"name": "${committerName}", "email": "${committerEmail}" }`
 const details = `"subject": "${subject}", "sanitizedSubject": "${message}", "body": "${body}"`
-const dateInfo = `"authoredOn": "${authorDate}", "committedOn": "${committerDate}", "body": "${body}"`
+const dateInfo = `"authoredOn": "${authorDate}", "committedOn": "${committerDate}"`
 
 const formatJSON = `{ "sha": "${sha}", "parents": "${parents}", ${author}, ${committer}, ${details}, ${dateInfo}},`
 // console.log('formatJSON', formatJSON)
@@ -43,14 +43,14 @@ const localGetCommits = (base, head) => {
 
       /* If JSON has new lines we must remove them  */
       // was /"body":\s+?("[\s\S]*?")/gm
-      const pattern = /("[\s\S]*?")/gm
+      const pattern = /("[\s\S\r]*?")/gm
       const matches = jsonValue.match(pattern)
-      const singleMatch = /("[\s\S]*?")/
+      const singleMatch = /("[\s\S\r]*?")/
       for (let i = 0; i < matches.length; i++) {
         const foundMatch = singleMatch.exec(matches[i])
         if (foundMatch && foundMatch[1]) {
           /* Replace all new lines */
-          // Trim outer quotes to fix inner quotes
+          // Trim outer quotes to fix inner quotes. Watch https://github.com/DavidWells/components/commit/2355073007f6a17d6a32235b1f4ddf5a2c2b90ff change
           let fixedValue = foundMatch[1].replace(/^"/, '').replace(/"$/, '')
             // foundMatch[1].substring(1, foundMatch[1].length - 1)
             /* Remove all new line characters & use \n placeholder */
@@ -58,7 +58,6 @@ const localGetCommits = (base, head) => {
             // Replace inner double quotes
             .replace(/"/gm, '\\"')
 
-          // console.log('result', fixedValue)
           // .replace(/"/gm, '\\"')
           /* Remove signed off by git messages */
           fixedValue = removeSignedOffBy(fixedValue)
@@ -73,10 +72,14 @@ const localGetCommits = (base, head) => {
       try {
         commits = JSON5.parse(asJSONString)
       } catch (err) {
-        console.log('JSON parse error')
-        console.log(err.message)
-        console.log(asJSONString)
-        throw new Error(err)
+        try {
+          commits = attemptToFix(asJSONString)
+        } catch (e) {
+          console.log('JSON parse error')
+          console.log(err.message)
+          console.log(asJSONString)
+          throw new Error(err)
+        }
       }
       realCommits = realCommits.concat(commits.map(c =>
         Object.assign(Object.assign({}, c), { parents: c.parents.split(' ') })
@@ -103,6 +106,56 @@ const localGetCommits = (base, head) => {
       }
     })
   })
+}
+
+const FIX_SUBJECT = /("subject":)([\s\S]*?)*?("sanitizedSubject")/g
+const FIX_BODY = /("body":)([\s\S]*?)*?("authoredOn")/g
+
+function attemptToFix(jsonLikeValue) {
+  const x = findAndFind(jsonLikeValue, FIX_SUBJECT)
+  return findAndFind(x, FIX_BODY)
+}
+
+function findAndFind(str, pattern) {
+  const x = getMatches(str, pattern)
+  let newX = str
+  for (let i = 0; i < x.length; i++) {
+    newX = newX.replace(x[i].match, x[i].replacement)
+  }
+  return newX
+}
+
+function getMatches(str, myRegex) {
+  var matches = []
+  var match
+
+  while (match = myRegex.exec(str)) { // eslint-disable-line
+    const firstKey = match[1]
+    const lastKey = match[3]
+    // console.log('match[0]', match[0])
+
+    const cleanSubject = match[0]
+      .replace(match[1], '')
+      .replace(match[3], '')
+      // .replace(/,\n\s*$/, '')
+      .replace(/(\r\n|\n|\r)/gm, '')
+      .replace(/"/gm, '')
+      .trim()
+      .replace(/,$/gm, '')
+    // console.log('cleanSubject', `"${cleanSubject}"`)
+    if (cleanSubject) {
+      matches.push({
+        match: match[0],
+        replacement: `${firstKey} "${removeSignedOffBy(cleanSubject)}",\n ${lastKey}`,
+      })
+    }
+
+    if (myRegex.lastIndex === match.index) {
+      myRegex.lastIndex++
+    }
+  }
+
+  return matches
 }
 
 module.exports.formatJSON = formatJSON
--- packages/util-git-info/src/git/localGetCommits.test.js
+++ packages/util-git-info/src/git/localGetCommits.test.js
@@ -2,7 +2,7 @@ const test = require('ava')
 const { formatJSON } = require('./localGetCommits')
 
 test('generates a JSON-like commit message', async (t) => {
-  t.deepEqual(formatJSON, '{ "sha": "%H", "parents": "%p", "author": {"name": "%an", "email": "%ae" }, "committer": {"name": "%cn", "email": "%ce" }, "subject": "%s", "sanitizedSubject": "%f", "body": "%b", "authoredOn": "%aI", "committedOn": "%cI", "body": "%b"},')
+  t.deepEqual(formatJSON, '{ "sha": "%H", "parents": "%p", "author": {"name": "%an", "email": "%ae" }, "committer": {"name": "%cn", "email": "%ce" }, "subject": "%s", "sanitizedSubject": "%f", "body": "%b", "authoredOn": "%aI", "committedOn": "%cI"},')
   const withoutComma = formatJSON.substring(0, formatJSON.length - 1)
   t.notThrows(() => JSON.parse(withoutComma))
 })
