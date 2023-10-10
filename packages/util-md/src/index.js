d00bcc360716fdde3126f96333849ae7efabdd61
David Wells <hello@davidwells.io>
2023-10-09 17:31:52 -0700

    feat: add removeH1 md util

--- packages/util-md/src/index.js
+++ packages/util-md/src/index.js
@@ -1,7 +1,23 @@
 const { parseMarkdown } = require('./parse')
 const { parseFrontmatter } = require('./frontmatter')
 
+const FIRST_H1_UNDERSCORE = /^(.*)\n*?===+/
+const FIRST_H1_HASH = /^# (.*)/m
+
+function removeLeadingH1(content) {
+  let finalContent = content
+  // Remove Leading h1 if exists
+  if (content.startsWith('# ')) {
+    finalContent = content.replace(FIRST_H1_HASH, '')
+  // Remove heading H1 if ========= format
+  } else if (content.match(FIRST_H1_UNDERSCORE)) {
+    finalContent = content.replace(FIRST_H1_UNDERSCORE, '')
+  }
+  return finalContent
+}
+
 module.exports = {
   parseMarkdown,
   parseFrontmatter,
+  removeLeadingH1,
 }
