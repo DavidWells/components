a386e44667318dd11386d994f0d42822b4bce0ec
DavidWells <hello@davidwells.io>
2025-02-06 13:39:32 -0800

    logger

--- packages/util-yaml/src/index.test.js
+++ packages/util-yaml/src/index.test.js
@@ -1015,19 +1015,37 @@ function read(filePath) {
   return fs.readFileSync(filePath, 'utf-8')
 }
 
-function deepLog(objOrLabel, logVal) {
-  let obj = objOrLabel
-  if (typeof objOrLabel === 'string') {
-    obj = logVal
-    const prefix = (arguments.length > 1) ? '> ' : ``
-    console.log(`\x1b[33m\n${prefix}${objOrLabel}\x1b[0m`)
-  }
-  if (typeof obj === 'object') {
-    console.log(util.inspect(obj, false, null, true))
+// function deepLog(objOrLabel, logVal) {
+//   let obj = objOrLabel
+//   if (typeof objOrLabel !== 'object') {
+//     obj = logVal
+//     const prefix = (arguments.length > 1) ? '> ' : ``
+//     console.log(`\x1b[33m\n${prefix}${objOrLabel}\x1b[0m`)
+//   }
+//   if (typeof obj === 'object') {
+//     console.log(util.inspect(obj, false, null, true))
+//     return
+//   }
+//   if (arguments.length <= 1) return
+//   console.log(arguments[1])
+// }
+
+function logValue(value, isFirst, isLast) {
+  const prefix = `${isFirst ? '> ' : ''}`
+  if (typeof value === 'object') {
+    console.log(`${prefix}${util.inspect(value, false, null, true)}\n`)
     return
   }
-  if (arguments.length <= 1) return
-  console.log(arguments[1])
+  if (isFirst) {
+    console.log(`\n\x1b[33m${prefix}${value}\x1b[0m`)
+    return
+  }
+  console.log((typeof value === 'string' && value.includes('\n')) ? `\`${value}\`` : value)
+  // isLast && console.log(`\x1b[37m\x1b[1m${'─'.repeat(94)}\x1b[0m\n`)
+}
+
+function deepLog() {
+  for (let i = 0; i < arguments.length; i++) logValue(arguments[i], i === 0, i === arguments.length - 1)
 }
 
 function getCfnSchema() {
